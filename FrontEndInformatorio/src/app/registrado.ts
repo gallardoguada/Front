3734380aa2c2696ead79589f5afcb674706796d7
leylaalfonzo96 <leylaalfonzo96@gmail.com>
2017-12-01 20:22:39 -0300

    registro7.0
    asdasfasf

--- FrontEndInformatorio/src/app/registrado.ts
+++ FrontEndInformatorio/src/app/registrado.ts
@@ -3,7 +3,7 @@ export class Registrado {
     constructor(
         public nombre: string,
         public apellido: string,
-        public dni: string,
+        public dni: number,
         public mail: string,
         public pass: string,
         public pass2: string
--- FrontEndInformatorio/src/app/registro/registro.component.ts
+++ FrontEndInformatorio/src/app/registro/registro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import{Registrado} from 'C:/Users/leyla/Desktop/Front/FrontEndInformatorio/src/app/registrado';
+import{Registrado} from 'C:/Front/FrontEndInformatorio/src/app/registrado';
 /*El import{registrado} importa (valga la redundancia) la clase registrado definido en campos.ts*/
 
 @Component({
@@ -15,10 +15,15 @@ import{Registrado} from 'C:/Users/leyla/Desktop/Front/FrontEndInformatorio/src/a
 export class RegistroComponent implements OnInit {
 
 
-  model = new Registrado ('','','','','','') /*Crea un MODELO de registrado y se le asigna valores iniciales según campos tenga*/
+  model = new Registrado ('','',null,'','','') /*Crea un MODELO de registrado y se le asigna valores iniciales según campos tenga*/
 
   submitted = false; /*variable submitted tipo booleana, inicializada en falso */
 
+  compararContrasenia(pass01, pass02){
+    if(pass01 != pass02){
+      console.log('pass pal culo');
+    }
+  }
 
   onSubmit(){ /*Función establecida que pone la variable submitted en verdadero, cuando se la llame */
     this.submitted = true;
