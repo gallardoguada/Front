2e991083862137a549769c30da590e1519e51b77
Alcoth <31553993+Alcoth@users.noreply.github.com>
2017-12-01 22:43:07 -0300

    componente de fotos agregado

--- FrontEndInformatorio/src/app/fotos/fotos.component.ts
+++ FrontEndInformatorio/src/app/fotos/fotos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var jquery:any;
+declare var $ :any; 
 
 @Component({
   selector: 'app-fotos',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fotos.component.css']
 })
 export class FotosComponent implements OnInit {
+  
 
+ Cargarfotos(){
+ (function(d, s, id){var js; if (d.getElementById(id)) 
+  {return;} js = d.createElement(s); 
+  js.id = id; js.src = "https://embedsocial.com/embedscript/ei.js"; 
+  d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialScript")); 
+}
+  
   constructor() { }
 
   ngOnInit() {
+    this.Cargarfotos()
+    
   }
 
 }
